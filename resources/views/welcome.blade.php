a7f64c10e69a48edcded21c9c0f1cc1b2bf80a51
Youness Bouallala <unessbouallala@gmail.com>
2024-10-06 20:46:00 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>SportSync</title>
+    <title>SportSync v1</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
